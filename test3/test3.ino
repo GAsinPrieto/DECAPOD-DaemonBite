687d7696847d3a356a3d55d3af333fad1b30ec78
GAsinPrieto <guillermo.asin.prieto@gmail.com>
2022-05-23 08:52:23 +0200

    tests PCE B mode

---
+++ test3/test3.ino
@@ -0,0 +1,16 @@
+void setup() {
+
+  //while(!(UDADDR & _BV(ADDEN))){ //check USB connection
+  //SerialNotInit=true;
+  DDRD  = B00000000;
+  PORTD |=  B00110110;
+  DDRF  = B00000000;
+  PORTF = B00000000;
+  //}
+}
+
+void loop() {
+
+  while (1);
+
+}
---
+++ test5/test5.ino
@@ -0,0 +1,18 @@
+void setup() {
+
+  //while(!(UDADDR & _BV(ADDEN))){ //check USB connection
+  //SerialNotInit=true;
+  DDRD  = B00000000;
+  //PORTD |=  B00110110;
+  DDRD  |=  B01000001;
+  PORTD &= ~B01000001;
+  DDRF  = B00000000;
+  PORTF = B00000000;
+  //}
+}
+
+void loop() {
+
+  while (1);
+
+}
