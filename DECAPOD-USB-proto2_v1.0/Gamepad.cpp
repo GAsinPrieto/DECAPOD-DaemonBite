5b06a4c6b7bc300aad7a27944150adaa683a2c3d
GAsinPrieto <guillermo.asin.prieto@gmail.com>
2022-08-22 08:46:56 +0200

    Update Gamepad.cpp

--- DECAPOD-USB-proto2_v1.0/Gamepad.cpp
+++ DECAPOD-USB-proto2_v1.0/Gamepad.cpp
@@ -150,17 +150,17 @@ static const uint8_t _hidReportDescriptorPCE[] PROGMEM = {
     
       0x05, 0x09,                       // USAGE_PAGE (Button)
       0x19, 0x01,                       // USAGE_MINIMUM (Button 1)
-      0x29, 0x04,                       // USAGE_MAXIMUM (Button 4)
+      0x29, 0x08,                       // USAGE_MAXIMUM (Button 8)
       0x15, 0x00,                       // LOGICAL_MINIMUM (0)
       0x25, 0x01,                       // LOGICAL_MAXIMUM (1)
-      0x95, 0x04,                       // REPORT_COUNT (4)
+      0x95, 0x08,                       // REPORT_COUNT (8)
       0x75, 0x01,                       // REPORT_SIZE (1)
       0x81, 0x02,                       // INPUT (Data,Var,Abs)
 
-      0x95, 0x01,                       // REPORT_COUNT (1) ; pad out the bits into a number divisible by 8
-      0x75, 0x04,                       // REPORT_SIZE (4)
+      /*0x95, 0x01,                       // REPORT_COUNT (1) ; pad out the bits into a number divisible by 8
+      0x75, 0x08,                       // REPORT_SIZE (8)
       0x81, 0x03,                       // INPUT (Const,Var,Abs)
-
+*/
       0x05, 0x01,                       // USAGE_PAGE (Generic Desktop)
       0x09, 0x01,                       // USAGE (pointer)
       0xa1, 0x00,                       // COLLECTION (Physical) 
